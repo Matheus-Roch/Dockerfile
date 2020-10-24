1c10602a100109b6c24fd88b679faca2571f9841
matheus-roch <matheus.grocha@aluno.faculdadeimpacta.com.br>
2020-10-23 22:22:02 -0300

    AC4

--- fibo.py
+++ fibo.py
@@ -2,17 +2,17 @@ def fibonacci():
     proximo = 1
     anterior = 0
     limite = 50
-    f = 0
+    found = 0
     resposta = "0,"
-    while (f < limite):
+    while (found < limite):
         tmp = proximo
         proximo = proximo + anterior
         anterior = tmp
-        f = f + 1
+        found = found + 1
         resposta+= str(proximo)+ ","
     
-    return(resposta)
+    return resposta
     
-    if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
