b9b70f579dd8d9966fc658c1518c3aedca9c3345
Matheus-Rocha <matheus.grocha@aluno.faculdadeimpacta.com.br>
2020-10-30 23:45:52 +0000

    AC4

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, jsonify, request
+from math import sqrt
+app = Flask(__name__)
+
+@app.route('/')
+def fibonacci():
+    proximo = 1
+    anterior = 0
+    limite = 50
+    found = 0
+    resposta = "0,"
+    while (found < limite):
+        tmp = proximo
+        proximo = proximo + anterior
+        anterior = tmp
+        found=found+1
+        resposta+= str(proximo)+ ","
+    
+    return resposta
+
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
